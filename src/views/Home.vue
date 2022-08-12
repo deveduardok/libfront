641ef4f8460f5db2075fc201c39af84b5dce74c5
deveduardok <deveduardok@gmail.com>
2022-08-11 23:10:27 -0300

    First commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-center">
-    <h1>Bem vindo!</h1>
-    <p>Esta é uma aplicação exemplo que utiliza autenticação com JWT</p>
+    <h1>BIBLIOTECA FATEC</h1>
+    <p>Bem vindo!</p>
   </div>
 </template>
 
